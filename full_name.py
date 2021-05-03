09c346e8734fad77203148112b06597548bb30f1
Nicole <nyarbrough14@gmail.com>
2021-05-03 15:59:31 -0700

    Created tests for full_name.py

--- full_name.py
+++ full_name.py
@@ -1,9 +1,14 @@
 def get_Full(first, last):
-    fullName = first + " " + last
+    #print("first:", first)
+    #print("last:", last)
+    fullName = str(first)
+    fullName = fullName + " "
+    fullName = fullName + str(last)
+    #print("full:", fullName)
     return fullName
 
-firstName = input("Enter your first name: ")
-lastName = input("Enter your last name: ")
+#firstName = input("Enter your first name: ")
+#lastName = input("Enter your last name: ")
 
-fullName = get_Full(firstName, lastName)
-print("Full name:", fullName)
+#fullName = get_Full(firstName, lastName)
+#print("Full name:", fullName)
--- test_full_name.py
+++ test_full_name.py
@@ -2,7 +2,17 @@ import unittest
 import full_name
 
 class testCaseAdd(unittest.TestCase):
-    pass
+    def test_normal(self):
+        self.assertEqual(full_name.get_Full("fred", "green"), "fred green")
+
+    def test_capitalization(self):
+        self.assertEqual(full_name.get_Full("bEnnY", "BeAVer"), "bEnnY BeAVer")
+
+    def test_format_int(self):
+        self.assertEqual(full_name.get_Full(23, 42), "23 42")
+
+    def test_format_float(self):
+        self.assertEqual(full_name.get_Full(5.6, 90.2), "5.6 90.2")
 
 if __name__ == '__main__':
     unittest.main()
