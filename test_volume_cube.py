6bc626d4008f757746548a946cceb9cf6cee4293
Nicole <nyarbrough14@gmail.com>
2021-05-03 15:43:37 -0700

    Update testing document to add more test cases

--- test_volume_cube.py
+++ test_volume_cube.py
@@ -2,13 +2,24 @@ import unittest
 import volume_cube
 
 class testCaseAdd(unittest.TestCase):
-    def test_add(self):
-        #result = volume_cube.calcVolume(2)
-        #self.assertEqual(result, 8.0)
+    def test_pass2(self):
         self.assertEqual(volume_cube.calcVolume(2), 8.0)
-        self.assertEqual(volume_cube.calcVolume(2), 8.0)
-        #self.assertEqual(volume_cube.calcVolume(2), 4.0)
-        #self.assertEqual(volume_cube.calcVolume(2), 6.0)
+        #self.assertEqual(volume_cube.calcVolume(2), 8.0)
+
+    def test_pass3_5(self):
+        self.assertEqual(volume_cube.calcVolume(3.5), 42.875)
+
+    def test_fail4(self):
+        self.assertEqual(volume_cube.calcVolume(4), 1)
+
+    def test_fail2_1(self):
+        self.assertEqual(volume_cube.calcVolume(2.1), 10)
+
+    def test_fail_inputstr(self):
+        self.assertEqual(volume_cube.calcVolume("abc"), 2)
+
+    def test_fail_inputchar(self):
+        self.assertEqual(volume_cube.calcVolume('z'), 2)
 
 if __name__ == '__main__':
     unittest.main()
--- volume_cube.py
+++ volume_cube.py
@@ -1,11 +1,12 @@
 def calcVolume( dimension ):
+    # error handle input
+    print("-----------------------------------------")
+    if ((type(dimension) != int) and type(dimension) != float):
+        return -1
+    print("pass, input:", dimension)
+
     volume = (dimension ** 3)
+    print("volume:", volume)
 
     #print("volume")
     return volume
-
-#measureDimension = input("Enter the length of a side of the cube: ")
-
-#volume = calcVolume(float(measureDimension))
-
-#print("Cube volume:", volume)
